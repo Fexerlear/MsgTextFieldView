257d580e3186f293f497dc87acf1f5d7874837f5
Fexerlear <feixuanling@icloud.com>
2016-01-21 17:56:26 +0800

    添加登陆框的文件，使用便利构造器使用

--- MsgTextFieldView.h
+++ MsgTextFieldView.h
@@ -1,13 +1,57 @@
 
+
+
+
+
+
+
+
+
+
 #import <UIKit/UIKit.h>
+
+typedef NS_ENUM(NSInteger, MsgTopViewIsHide) {
+    
+    MsgTopViewIsHideHide = 0, // 隐藏上边线
+    MsgTopViewIsHideShow = 1 // 显示上边线
+    
+};
+
+typedef NS_ENUM(NSInteger, MsgBottomViewIsHide) {
+    MsgBottomViewIsHideShow = -1,
+    MsgBottomViewIsHideHide = 0
+    
+};
+
+
+
 
 @interface MsgTextFieldView : UIView
 
+@property (nonatomic, strong) UILabel * label; // 控件的label
+@property (nonatomic, strong) UITextField  * textfield; // 控件的输入框
+@property (nonatomic, strong) UIColor * lineColor; // 上线边线的颜色
 
-+ (instancetype *)msgTextFieldViewWithLabelTitle:(NSString *)title textplaceholder:(NSString *)textplaceholder;
+@property (nonatomic, assign) UITextFieldViewMode textfieldMode; // textfield的消除图标
+@property (nonatomic, assign) BOOL clearsOnBeginEditing; // 点击文本框是否删除上次输入内容
 
 
-
+/**
+ *  便利构造器创建控件
+ *
+ *  @param frame           控件的位置
+ *  @param labelFont       控件中label的字体
+ *  @param labeltextColor  控件中label的文字颜色
+ *  @param textFont        控件中textfield的字体
+ *  @param title           控件中label的标题
+ *  @param textplaceholder 控件中textfield的placeholder的文字
+ *  @param secureTextEntry 是否明文显示textfield的文字
+ *  @param topHide         用来实现控件中 上边线 左部是否隐藏
+ *  @param bottomHide      用来实现控件中 下边线 左部是否隐藏
+ *
+ *  @return 返回需要的控件
+ */
++ (instancetype)msgTextFieldViewWithFrame:(CGRect)frame labelFont:(CGFloat)labelFont labeltextColor:(UIColor *)labeltextColor textFont:(CGFloat)textFont labelTitle:(NSString *)title textplaceholder:(NSString *)textplaceholder textsecureTextEntry:(BOOL)secureTextEntry viewTopHide:(MsgTopViewIsHide)topHide viewBottomHide:(MsgBottomViewIsHide)bottomHide;
 
 
 
--- MsgTextFieldView.m
+++ MsgTextFieldView.m
@@ -1,62 +1,142 @@
 
 
+
+
+
+
+
+
+
+
 #import "MsgTextFieldView.h"
+
+
+@interface MsgTextFieldView ()
+
+@property (nonatomic, strong) UIView * backgroundView;
+
+
+
+@end
+
 
 @implementation MsgTextFieldView
 
 
-- (void)drawRect:(CGRect)rect {
-    // Drawing code
+//- (void)drawRect:(CGRect)rect {
+//    // Drawing code
+//
+//
+//
+//}
+
+
+
+- (instancetype)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        
+        
+    }
+    return self;
+}
+
+- (instancetype)initWithCoder:(NSCoder *)coder
+{
+    self = [super initWithCoder:coder];
+    if (self) {
+        
+        
+    }
+    return self;
+}
+
+- (instancetype)initWithFrame:(CGRect)frame labelFont:(CGFloat)labelFont labeltextColor:(UIColor *)labeltextColor textFont:(CGFloat)textFont labelTitle:(NSString *)title textplaceholder:(NSString *)textplaceholder textsecureTextEntry:(BOOL)secureTextEntry viewTopHide:(MsgTopViewIsHide)topHide viewBottomHide:(MsgBottomViewIsHide)bottomHide {
     
-    UIView * backgroundView = [[UIView alloc]init];
-    backgroundView.backgroundColor = APP_Line_COLOR;
-    [self addSubview:backgroundView];
-    [backgroundView mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.top.equalTo(self.mas_bottom).offset(0);
+    self = [super initWithFrame:frame];
+    if (self) {
+        
+        [self loadselfViewWithFrame:frame labelFont:labelFont labeltextColor:(UIColor *)labeltextColor textFont:textFont labelTitle:title textplaceholder:textplaceholder textsecureTextEntry:secureTextEntry viewTopHide:topHide viewBottomHide:bottomHide];
+        
+    }
+    return self;
+    
+}
+
+- (void)loadselfViewWithFrame:(CGRect)frame labelFont:(CGFloat)labelFont labeltextColor:(UIColor *)labeltextColor textFont:(CGFloat)textFont labelTitle:(NSString *)title textplaceholder:(NSString *)textplaceholder textsecureTextEntry:(BOOL)secureTextEntry viewTopHide:(MsgTopViewIsHide)topHide viewBottomHide:(MsgBottomViewIsHide)bottomHide {
+    
+    
+    
+    // 预处理
+    if (!labelFont) {
+        labelFont = 17.0f;
+    }
+    
+    if (!textFont) {
+        textFont = labelFont;
+    }
+    
+    if (!labeltextColor) {
+        labeltextColor = [UIColor redColor];
+    }
+    
+    if (!topHide) {
+        topHide = MsgTopViewIsHideShow;
+    }
+    
+    if (!bottomHide) {
+        bottomHide = MsgBottomViewIsHideShow;
+    }
+    
+    _backgroundView = [[UIView alloc]init];
+    _backgroundView.backgroundColor = APP_Line_COLOR_d8;
+    [self addSubview:_backgroundView];
+    [_backgroundView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(self.mas_top).offset(0);
         make.left.equalTo(self.mas_left).offset(0);
         make.right.equalTo(self.mas_right).offset(0);
         make.bottom.equalTo(self.mas_bottom).offset(0);
-
+        
     }];
     
     UIView * leftView = [[UIView alloc]init];
     leftView.backgroundColor = [UIColor whiteColor];
-    [backgroundView addSubview:leftView];
+    [_backgroundView addSubview:leftView];
     [leftView mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.top.equalTo(backgroundView.mas_top).offset(1);
-        make.left.equalTo(backgroundView.mas_left).offset(0);
-        make.bottom.equalTo(backgroundView.mas_bottom).offset(-1);
+        make.top.equalTo(_backgroundView.mas_top).offset(topHide);
+        make.left.equalTo(_backgroundView.mas_left).offset(0);
+        make.bottom.equalTo(_backgroundView.mas_bottom).offset(bottomHide);
         make.width.equalTo(@20);
     }];
     
-    UILabel * accountLabel = [self setupLabelWithTitle:@"账号"];
-    [backgroundView addSubview:accountLabel];
-    [accountLabel mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.top.equalTo(backgroundView.mas_top).offset(1);
+    _label = [self setupLabelWithTitle:title];
+    _label.font = [UIFont systemFontOfSize:labelFont];
+    _label.textColor = labeltextColor;
+    
+    [_backgroundView addSubview:_label];
+    [_label mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(_backgroundView.mas_top).offset(1);
         make.left.equalTo(leftView.mas_right).offset(0);
-        make.width.equalTo(@50);
-        make.height.equalTo(self.mas_height);
+        make.width.equalTo(@90);
+        make.height.equalTo(self.mas_height).offset(-2);
     }];
     
     
+    _textfield = [self setupTextFieldWithPlaceholder:textplaceholder];
+    _textfield.secureTextEntry = secureTextEntry;
+    _textfield.font = [UIFont systemFontOfSize:textFont];
     
-    UITextField * accountTextField = [self setupTextFieldWithPlaceholder:@"请输入您注册的手机号"];
-    [backgroundView addSubview:accountTextField];
-    [accountTextField mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.top.equalTo(backgroundView.mas_top).offset(1);
-        make.left.equalTo(accountLabel.mas_right).offset(0);
-        make.right.equalTo(backgroundView.mas_right).offset(0);
-        make.height.equalTo(self.mas_height);
+    [_backgroundView addSubview:_textfield];
+    [_textfield mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(_backgroundView.mas_top).offset(1);
+        make.left.equalTo(_label.mas_right).offset(0);
+        make.right.equalTo(_backgroundView.mas_right).offset(0);
+        make.height.equalTo(self.mas_height).offset(-2);
     }];
-
-    accountTextField.clearButtonMode = UITextFieldViewModeWhileEditing;
     
-
-    
-    
-    
+    _textfield.clearButtonMode = UITextFieldViewModeWhileEditing;
 }
-
 
 #pragma mark - 创建label
 - (UILabel *)setupLabelWithTitle:(NSString *)title
@@ -65,8 +145,6 @@
     label.backgroundColor = [UIColor whiteColor];
     label.text = title;
     label.textAlignment = NSTextAlignmentLeft;
-    label.textColor = Main_Text_Color;
-    label.font = [UIFont systemFontOfSize:14];
     return label;
 }
 
@@ -76,42 +154,29 @@
     UITextField * textField = [[UITextField alloc]init];
     textField.backgroundColor = [UIColor whiteColor];
     textField.placeholder = placeholder;
-    textField.font = [UIFont systemFontOfSize:12];
     return textField;
 }
 
 
+- (void)setLineColor:(UIColor *)lineColor {
+    
+    _backgroundView.backgroundColor = lineColor;
+}
 
+- (void)setTextfieldMode:(UITextFieldViewMode)textfieldMode {
+    
+    _textfield.clearButtonMode = textfieldMode;
+}
 
+- (void)setClearsOnBeginEditing:(BOOL)clearsOnBeginEditing {
+    _textfield.clearsOnBeginEditing = clearsOnBeginEditing;
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
++ (instancetype)msgTextFieldViewWithFrame:(CGRect)frame labelFont:(CGFloat)labelFont labeltextColor:(UIColor *)labeltextColor textFont:(CGFloat)textFont labelTitle:(NSString *)title textplaceholder:(NSString *)textplaceholder textsecureTextEntry:(BOOL)secureTextEntry viewTopHide:(MsgTopViewIsHide)topHide viewBottomHide:(MsgBottomViewIsHide)bottomHide {
+    
+    return  [[[self class] alloc] initWithFrame:frame labelFont:labelFont labeltextColor:(UIColor *)labeltextColor textFont:textFont labelTitle:title textplaceholder:textplaceholder textsecureTextEntry:secureTextEntry viewTopHide:topHide viewBottomHide:bottomHide];
+    
+}
 
 
 
